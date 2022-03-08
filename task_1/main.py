5b196e61d999d50858d11c836e9f2e0a05a78c80
AntonNov <anton.nov123@gmail.com>
2022-03-08 19:03:00 +0300

    started write function of finding Ngrams

--- task_1/main.py
+++ task_1/main.py
@@ -9,7 +9,7 @@ from collections import defaultdict
 from numpy import median, mean
 
 
-def word_occurs():
+def word_occurs_in_text():
     with open('file.txt') as f:
         text_in_words = []
         for line in f:
@@ -21,16 +21,24 @@ def word_occurs():
             print(f"Слово \"{key}\" встречается {value} раз")
 
 
-def words_in_a_sentence():
+def words_in_sentence():
     with open('file.txt') as f:
         my_string = str()
         for line in f:
             my_string += line
-        for el in ["?", "!", "...", "?!"]:
+        for el in ("?", "!", "...", "?!", "!?"):
             my_string = my_string.replace(el, ".")
         sentences = my_string.split(".")
         print("Медианное количество слов в предложении: ", median([len(sentence.split()) for sentence in sentences]))
         print("Среднее количество слов в предложении: ", mean([len(sentence.split()) for sentence in sentences]))
+
+
+def fins_Ngrams(K=10, N=4):
+    with open('file.txt') as f:
+        Ngrams = str()
+        for i in range(len(N)):
+            Ngrams += f[i]
+        print(Ngrams)
 
 
 if __name__ == "__main__":
@@ -39,18 +47,19 @@ if __name__ == "__main__":
             value = int(input("Тыкните, что хотите посмотреть в этом тексте:\n"
                               "1. Статистика по словам в тексте\n"
                               "2. Статистика по словам в предложении\n"
+                              "3. Поиск N-грам"
                               ))
             match value:
                 case 1:
-                    word_occurs()
+                    word_occurs_in_text()
                     break
                 case 2:
-                    words_in_a_sentence()
+                    words_in_sentence()
                     break
                 case _:
                     print("Вы промахнулись, нажмите цифру ещё раз")
         is_final = None
-        while not is_final in ["y", "n"]:
+        while not is_final in ("y", "n"):
             is_final = str(input("Уходите? y/n\n"))
         if is_final == "y":
             break
