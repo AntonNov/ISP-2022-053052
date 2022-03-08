bd47e1c7d48cc224465e896c8908f8519f4ce337
AntonNov <anton.nov123@gmail.com>
2022-03-08 23:54:36 +0300

    refactoring

--- task_1/main.py
+++ task_1/main.py
@@ -11,34 +11,34 @@ from numpy import median, mean
 
 def word_occurs_in_text():
     with open('file.txt') as f:
-        text_in_words = []
+        text_in_words = list()
         for line in f:
             text_in_words += line.split()
-        my_dict = defaultdict(int)
+        stats = defaultdict(int)
         for word in text_in_words:
-            my_dict[word] += 1
-        for key, value in my_dict.items():
+            stats[word] += 1
+        for key, value in stats.items():
             print(f"Слово \"{key}\" встречается {value} раз")
 
 
 def words_in_sentence():
     with open('file.txt') as f:
-        my_string = str()
+        text = str()
         for line in f:
-            my_string += line
+            text += line
         for el in ("?", "!", "...", "?!", "!?"):
-            my_string = my_string.replace(el, ".")
-        sentences = my_string.split(".")
+            text = text.replace(el, ".")
+        sentences = text.split(".")
         print("Медианное количество слов в предложении: ", median([len(sentence.split()) for sentence in sentences]))
         print("Среднее количество слов в предложении: ", mean([len(sentence.split()) for sentence in sentences]))
 
 
-def fins_Ngrams(K=10, N=4):
+def find_Ngrams(K=10, N=4):
     with open('file.txt') as f:
-        Ngrams = str()
-        for i in range(len(N)):
-            Ngrams += f[i]
-        print(Ngrams)
+        text = str()
+        for line in f:
+            text += line
+        print(text)
 
 
 if __name__ == "__main__":
@@ -47,14 +47,19 @@ if __name__ == "__main__":
             value = int(input("Тыкните, что хотите посмотреть в этом тексте:\n"
                               "1. Статистика по словам в тексте\n"
                               "2. Статистика по словам в предложении\n"
-                              "3. Поиск N-грам"
-                              ))
+                              "3. Поиск N-грам\n"
+                              )
+                        )
+
             match value:
                 case 1:
                     word_occurs_in_text()
                     break
                 case 2:
                     words_in_sentence()
+                    break
+                case 3:
+                    find_Ngrams()
                     break
                 case _:
                     print("Вы промахнулись, нажмите цифру ещё раз")
