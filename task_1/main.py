f4858b9e42915ad44787615abde19c7019c7074a
AntonNov <anton.nov123@gmail.com>
2022-03-17 20:26:25 +0300

    немного до  о final version

--- task_1/main.py
+++ task_1/main.py
@@ -7,12 +7,12 @@ top-K самых часто повторяющихся буквенных N-гр
 и встроенные операции над строками.
 Предусмотреть обработку знаков препинания."""
 from collections import defaultdict
-from numpy import median, mean
 from os import path
 import string
 import re
+from numpy import median, mean
 
-statistics = defaultdict(int)
+STATISTICS = defaultdict(int)
 
 
 def find_info_about_word_occurs_in_text() -> None:
@@ -30,28 +30,28 @@ def find_info_about_word_occurs_in_text() -> None:
                     """
                     for punct_mark in "[⇨]", "...«»" + string.punctuation:
                         if word.count(punct_mark) == 1:
-                            if punct_mark == word[-len(punct_mark) :]:
+                            if punct_mark == word[-len(punct_mark):]:
                                 word = word[: -len(punct_mark)]
                             elif punct_mark == word[: len(punct_mark)]:
-                                word = word[len(punct_mark) :]
+                                word = word[len(punct_mark):]
                         elif (
-                            word.count(punct_mark) == 2
-                            and word[: len(punct_mark)] == word[: -len(punct_mark)]
+                                word.count(punct_mark) == 2
+                                and word[: len(punct_mark)] == word[: -len(punct_mark)]
                         ):
-                            word = word[len(punct_mark) : -len(punct_mark)]
+                            word = word[len(punct_mark): -len(punct_mark)]
                     # while re.find(r"[.*]", word):
                     #    re.sub(r"[.*]", "", word)
                     if word != "—":
                         # print(f"Cтало \"{word}\"")
                         text_in_words.append(word)
-            global statistics
+            global STATISTICS
             for word in text_in_words:
-                statistics[word] += 1
-            statistics = dict(
-                sorted(statistics.items(), reverse=True, key=lambda x: x[1])
+                STATISTICS[word] += 1
+            STATISTICS = dict(
+                sorted(STATISTICS.items(), reverse=True, key=lambda x: x[1])
             )
             with open("output.txt", "w") as file2:
-                for key, value in statistics.items():
+                for key, value in STATISTICS.items():
                     file2.write(f'Слово "{key}" встречается {value} раз\n')
         else:
             print("Файл пустой!")
@@ -64,11 +64,11 @@ def find_info_about_words_in_sentence() -> None:
             for line in f:
                 text += line.strip()
             for el in (
-                "!",
-                "?",
-                "...",
-                "?!",
-                "!?",
+                    "!",
+                    "?",
+                    "...",
+                    "?!",
+                    "!?",
             ):
                 re.sub(f"{el}+", ".", text)
             sentences = text.split(".")
@@ -85,11 +85,11 @@ def find_info_about_words_in_sentence() -> None:
 
 
 def find_ngrams(k=10, n=4) -> None:
-    choice = None
-    global statistics
-    while choice not in ("y", "n"):
-        choice = input("Вы хотите использовать значения по умолчанию?(y/n)")
-    if choice == "n":
+    CHOICE = None
+    global STATISTICS
+    while CHOICE not in ("y", "n"):
+        CHOICE = input("Вы хотите использовать значения по умолчанию?(y/n)")
+    if CHOICE == "n":
         k, n = map(int, input("Введите k, n")).split()
     with open("input.txt") as f:
         if path.getsize("input.txt"):
@@ -97,11 +97,11 @@ def find_ngrams(k=10, n=4) -> None:
             for line in f:
                 text += line.strip()
             with open("output.txt", "w") as file2:
-                cnt = 0
-                keys = len(statistics.keys())
-                while cnt < k:
-                    file2.write(keys[cnt])
-                    cnt += 1
+                for el in [
+                    list(STATISTICS.keys()[i])
+                    for i in range(k) in list(STATISTICS.keys())
+                ]:
+                    file2.write(el)
         else:
             print("Файл пустой!")
 
@@ -122,8 +122,8 @@ if __name__ == "__main__":
                 "3. Поиск N-грам\n"
             )
         options[choice]()
-        is_final = None
-        while is_final not in ("y", "n"):
-            is_final = input("Уходите? y/n\n")
-        if is_final == "y":
+        IS_FINAL = None
+        while IS_FINAL not in ("y", "n"):
+            IS_FINAL = input("Уходите? y/n\n")
+        if IS_FINAL == "y":
             break
